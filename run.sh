8ceffc560025d79048d9c13fa16cbe299e6c612f
Dust Appeal <dustappeal@gmail.com>
2015-12-01 01:14:13 -0800

    Add shell script to download data.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+wget http://download.geonames.org/export/dump/cities1000.zip
+unzip cities1000.zip
+
+wget https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat
